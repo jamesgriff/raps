3303b5862a7ae0f0786a85ceebd1be202c2f2ff0
James Griffiths <james@jwgsoftware.com>
2022-07-29 18:13:37 +0100

    files just in data folder

--- pipelines/my-first-rap/1 - My first R script.R
+++ pipelines/my-first-rap/1 - My first R script.R
@@ -1,8 +1,8 @@
 
 print('About to load CSV')
-data <- read.csv('data/input/school-teacher-workforce.csv')
+data <- read.csv('data/school-teacher-workforce.csv')
 print('Loaded CSV')
 
 print('About to save CSV')
-write.csv(data, 'data/output/copy-of-school-teacher-workforce.csv')
+write.csv(data, 'data/copy-of-school-teacher-workforce.csv')
 print('Saved CSV')
