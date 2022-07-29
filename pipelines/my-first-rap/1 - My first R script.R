b195c2d05eb6789b3448e0e40f9b58f963f2b51e
James Griffiths <james@jwgsoftware.com>
2022-07-29 19:06:04 +0100

    save to different file name

--- pipelines/my-first-rap/1 - My first R script.R
+++ pipelines/my-first-rap/1 - My first R script.R
@@ -4,5 +4,5 @@ data <- read.csv('data/school-teacher-workforce.csv')
 print('Loaded CSV')
 
 print('About to save CSV')
-write.csv(data, 'data/copy-of-school-teacher-workforce.csv')
+write.csv(data, 'data/school-teacher-workforce-2.csv')
 print('Saved CSV')
