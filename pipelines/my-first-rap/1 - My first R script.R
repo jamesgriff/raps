768e150b281e5886cda52b9a51b0f14e86e6ebf5
James Griffiths <james@jwgsoftware.com>
2022-08-01 11:27:37 +0100

    Add more comments

--- pipelines/my-first-rap/1 - My first R script.R
+++ pipelines/my-first-rap/1 - My first R script.R
@@ -1,8 +1,10 @@
 
-print('About to load CSV')
+print('Welcome to script 1')
+
+print('About to load CSV: school-teacher-workforce.csv')
 data <- read.csv('data/school-teacher-workforce.csv')
 print('Loaded CSV')
 
-print('About to save CSV')
+print('About to save CSV: school-teacher-workforce-1.csv')
 write.csv(data, 'data/school-teacher-workforce-1.csv')
 print('Saved CSV')
--- pipelines/my-first-rap/2 - My second R script.R
+++ pipelines/my-first-rap/2 - My second R script.R
@@ -1,8 +1,10 @@
 
-print('About to load CSV')
+print('Welcome to script 2')
+
+print('About to load CSV: school-teacher-workforce-1.csv')
 data <- read.csv('data/school-teacher-workforce-1.csv')
 print('Loaded CSV')
 
-print('About to save CSV')
+print('About to save CSV: school-teacher-workforce-2.csv')
 write.csv(data, 'data/school-teacher-workforce-2.csv')
 print('Saved CSV')
